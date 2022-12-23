28c35298e03270c7866e8a1703aa215f3d04ccd0
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2022-12-23 09:00:06 -0300

    refactor: change function declaration

--- chapter1/lib/algorithm.dart
+++ chapter1/lib/algorithm.dart
@@ -9,10 +9,11 @@ String statement(Invoice invoice, Map<String, Play> plays) {
   int volumeCredits = 0;
   String result = ' Statement for ${invoice.customer}\n';
 
-  int amountFor(Play? play, Performance perf) {
+  Play? playFor(Performance aPerformance) => plays[aPerformance.playID];
+  int amountFor(Performance perf) {
     int result = 0;
     
-    switch (play?.type) {
+    switch (playFor(perf)?.type) {
       case 'tragedy':
         result = 40000;
         if (perf.audience > 30) {
@@ -27,17 +28,15 @@ String statement(Invoice invoice, Map<String, Play> plays) {
         break;
       default:
         result += 300 * perf.audience;
-        throw Exception('unknown type: ${play?.type}');
+        throw Exception('unknown type: ${playFor(perf)?.type}');
     }
     return result;
   }
-
-  Play? playFor(Performance aPerformance) => plays[aPerformance.playID];
   
   final format = NumberFormat.currency(locale: 'en-US', symbol: 'USD');
 
   for (var perf in invoice.performances) {
-    final thisAmount = amountFor(playFor(perf), perf);
+    final thisAmount = amountFor(perf);
     // add volume credits
     volumeCredits += max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
