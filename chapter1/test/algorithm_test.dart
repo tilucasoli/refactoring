28df5c986af77cb55866def755ff88ac424a5207
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2022-12-25 16:22:26 -0300

    Improve test case

--- chapter1/test/algorithm_test.dart
+++ chapter1/test/algorithm_test.dart
@@ -6,23 +6,35 @@ import 'package:test/test.dart';
 void main() {
   final plays = {
     'hamlet': Play(name: 'Hamlet', type: 'tragedy'),
+    'as-like': Play(name: 'As You Like It', type: 'comedy'),
+    'othello': Play(name: 'Othello', type: 'tragedy'),
   };
 
   final invoice = Invoice(
     customer: 'BigCo',
     performances: [
-      Performance(
-        playID: 'hamlet',
-        audience: 55,
-      ),
-    ],
+        Performance(
+          playID: 'hamlet',
+          audience: 55,
+        ),
+        Performance(
+          playID: 'as-like',
+          audience: 35,
+        ),
+        Performance(
+          playID: 'othello',
+          audience: 40,
+        ),
+      ],
   );
 
   const expectedResult = '''
  Statement for BigCo
  Hamlet: USD650.00 (55 seats)
-Amount owed is USD650.00
-You earned 25 credits
+ As You Like It: USD475.00 (35 seats)
+ Othello: USD500.00 (40 seats)
+Amount owed is USD1,625.00
+You earned 47 credits
 ''';
 
   test('Statement', () {
