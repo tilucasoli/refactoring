0343e37a220beeb15fbb1de523ad094cb47e6598
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2022-12-28 09:06:19 -0300

    refactor split phases
    Entre renderPlainText() e statement() ele sugeriu um camada intermediaria que funcionaria com comunicação entre ambas

--- chapter1/lib/algorithm.dart
+++ chapter1/lib/algorithm.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:math';
 
 import 'package:chapter1/model/invoice.dart';
@@ -5,17 +6,29 @@ import 'package:chapter1/model/play.dart';
 import 'package:intl/intl.dart';
 
 String statement(Invoice invoice, Map<String, Play> plays) {
-  return renderPlainText(invoice, plays);
+  Play? playFor(Performance aPerformance) => plays[aPerformance.playID];
+
+  PerformanceEnrich enrichPerformance(Performance aPerformance) =>
+      PerformanceEnrich(
+        play: playFor(aPerformance),
+        audience: aPerformance.audience,
+        playID: aPerformance.playID,
+      );
+
+  final data = StatementData(
+    customer: invoice.customer,
+    performances:
+        invoice.performances.map((e) => enrichPerformance(e)).toList(),
+  );
+
+  return renderPlainText(data, plays);
 }
 
-String renderPlainText(Invoice invoice, Map<String, Play> plays) {
-  
-  Play? playFor(Performance aPerformance) => plays[aPerformance.playID];
-  
-  int amountFor(Performance aPerfomance) {
+String renderPlainText(StatementData data, Map<String, Play> plays) {
+  int amountFor(PerformanceEnrich aPerfomance) {
     int result = 0;
-  
-    switch (playFor(aPerfomance)?.type) {
+
+    switch (aPerfomance.play?.type) {
       case 'tragedy':
         result = 40000;
         if (aPerfomance.audience > 30) {
@@ -30,41 +43,41 @@ String renderPlainText(Invoice invoice, Map<String, Play> plays) {
         break;
       default:
         result += 300 * aPerfomance.audience;
-        throw Exception('unknown type: ${playFor(aPerfomance)?.type}');
+        throw Exception('unknown type: ${aPerfomance.play?.type}');
     }
     return result;
   }
-  
-  int volumeCreditsFor(Performance perf) {
+
+  int volumeCreditsFor(PerformanceEnrich perf) {
     int result = 0;
     result += max(perf.audience - 30, 0);
-    if ('comedy' == playFor(perf)?.type) result += (perf.audience / 5).floor();
+    if ('comedy' == perf.play?.type) result += (perf.audience / 5).floor();
     return result;
   }
-  
+
   int totalVolumeCredits() {
     int result = 0;
-    for (var perf in invoice.performances) {
+    for (var perf in data.performances) {
       result += volumeCreditsFor(perf);
     }
     return result;
   }
-  
+
   int totalAmount() {
     int result = 0;
-    for (var perf in invoice.performances) {
+    for (var perf in data.performances) {
       result += amountFor(perf);
     }
     return result;
   }
-  
-  String result = ' Statement for ${invoice.customer}\n';
-  
-  for (var perf in invoice.performances) {
+
+  String result = ' Statement for ${data.customer}\n';
+
+  for (var perf in data.performances) {
     result +=
-        ' ${playFor(perf)?.name}: ${formatToUSD(amountFor(perf))} (${perf.audience} seats)\n';
+        ' ${perf.play?.name}: ${formatToUSD(amountFor(perf))} (${perf.audience} seats)\n';
   }
-  
+
   result += 'Amount owed is ${formatToUSD(totalAmount())}\n';
   result += 'You earned ${totalVolumeCredits()} credits\n';
   return result;
@@ -73,4 +86,26 @@ String renderPlainText(Invoice invoice, Map<String, Play> plays) {
 String formatToUSD(dynamic number) {
   var formatter = NumberFormat.currency(locale: 'en-US', symbol: 'USD');
   return formatter.format(number / 100);
+}
+
+class StatementData {
+  final String customer;
+  final List<PerformanceEnrich> performances;
+
+  StatementData({
+    required this.customer,
+    required this.performances,
+  });
+}
+
+class PerformanceEnrich {
+  final String playID;
+  final int audience;
+  final Play? play;
+
+  PerformanceEnrich({
+    required this.playID,
+    required this.audience,
+    required this.play,
+  });
 }
