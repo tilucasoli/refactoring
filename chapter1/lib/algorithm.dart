0e5fc2cb9cb3bc0352fe0f70d17dad0939e5357c
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2022-12-29 10:00:20 -0300

    refactor: extract function

--- chapter1/lib/algorithm.dart
+++ chapter1/lib/algorithm.dart
@@ -4,6 +4,14 @@ import 'package:chapter1/model/invoice.dart';
 import 'package:chapter1/model/play.dart';
 import 'package:intl/intl.dart';
 
+// Qual as responsabilidades da função?
+// [ ] - Calcular o valor total da conta
+// [ ] - Calcular o valor total de créditos
+// [ ] - Formatar exibição do resultado
+
+// Qual deveria ser o objetivo central da função?
+// [ ] - Retornar exibição do resultado no formato String 
+
 String statement(Invoice invoice, Map<String,Play> plays) {
   int totalAmount = 0;
   int volumeCredits = 0;
@@ -11,8 +19,12 @@ String statement(Invoice invoice, Map<String,Play> plays) {
 
   final format = NumberFormat.currency(locale: 'en-US', symbol: 'USD');
 
+  Play? playFor(Performance perf) {
+    return plays[perf.playID];
+  }
+
   for (var perf in invoice.performances) {
-    var play = plays[perf.playID];
+    var play = playFor(perf);
     int thisAmount = 0;
     switch (play?.type) {
       case 'tragedy':
