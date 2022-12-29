ce2e2d7acfe493313f99262e134dd6e218ee67d7
Lucas Oliveira <62367544+tilucasoli@users.noreply.github.com>
2022-12-29 10:42:20 -0300

    refactor: extract function

--- chapter1/lib/algorithm.dart
+++ chapter1/lib/algorithm.dart
@@ -5,14 +5,16 @@ import 'package:chapter1/model/play.dart';
 import 'package:intl/intl.dart';
 
 // Qual as responsabilidades da função?
-// [ ] - Calcular o valor total da conta
+// [x] - Calcula valor por performance
+// [ ] - Calcula valor de creditos por performance
+// [ ] - Calcular o valor total da conta total
 // [ ] - Calcular o valor total de créditos
 // [ ] - Formatar exibição do resultado
 
 // Qual deveria ser o objetivo central da função?
-// [ ] - Retornar exibição do resultado no formato String 
+// [ ] - Retornar exibição do resultado no formato String
 
-String statement(Invoice invoice, Map<String,Play> plays) {
+String statement(Invoice invoice, Map<String, Play> plays) {
   int totalAmount = 0;
   int volumeCredits = 0;
   String result = ' Statement for ${invoice.customer}\n';
@@ -23,31 +25,38 @@ String statement(Invoice invoice, Map<String,Play> plays) {
     return plays[perf.playID];
   }
 
-  for (var perf in invoice.performances) {
-    int thisAmount = 0;
+  int amountFor(Performance perf) {
+    int result = 0;
     switch (playFor(perf)?.type) {
       case 'tragedy':
-        thisAmount = 40000;
+        result = 40000;
         if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
+          result += 1000 * (perf.audience - 30);
         }
         break;
       case 'comedy':
-        thisAmount = 30000;
+        result = 30000;
         if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
+          result += 10000 + 500 * (perf.audience - 20);
         }
         break;
       default:
-        thisAmount += 300 * perf.audience;
+        result += 300 * perf.audience;
         throw Exception('unknown type: ${playFor(perf)?.type}');
     }
+    return result;
+  }
+
+  for (var perf in invoice.performances) {
+    int thisAmount = amountFor(perf);
     // add volume credits
     volumeCredits += max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if ('comedy' == playFor(perf)?.type) volumeCredits += (perf.audience / 5).floor();
+    if ('comedy' == playFor(perf)?.type)
+      volumeCredits += (perf.audience / 5).floor();
     // print line for this order
-    result += ' ${playFor(perf)?.name}: ${format.format(thisAmount / 100)} (${perf.audience} seats)\n';
+    result +=
+        ' ${playFor(perf)?.name}: ${format.format(thisAmount / 100)} (${perf.audience} seats)\n';
     totalAmount += thisAmount;
   }
   result += 'Amount owed is ${format.format(totalAmount / 100)}\n';
